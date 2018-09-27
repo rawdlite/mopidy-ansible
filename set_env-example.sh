2c1a7fead4a3671278cf2539e7343178b7b9feae
tom roth <t.roth@moebel-dvorak.de>
2018-09-27 09:45:16 +0200

    Hifishield Plus added

---
+++ set_env-example.sh
@@ -0,0 +1,15 @@
+export AUDIO_MIXER=yamaha
+export AUDIO_OUTPUT=autoaudiosink
+export BEETSLIBRARY=/data/music/music_var/beetslibrary.blb
+export BB_DATA_DIR=/data/music/music_var/bigbeet
+export PLAYLIST_DIR=/data/music/music_var/playlists
+export SOUNDCLOUD_AUTH_TOKEN=
+export G_MUSIC_USERNAME=
+export G_MUSIC_PASSWORD=
+export G_MUSIC_DEVICEID=
+export SPOTIFY_USERNAME=
+export SPOTIFY_PASSWORD=
+export SPOTIFY_CLIENT_ID=
+export SPOTIFY_CLIENT_SECRET=
+export YAMAHA_HOST=192.168.1.10
+export YAMAHA_SOURCE=AV3
